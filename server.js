e3288fd7fe8d39871d3443b896a7348aa537ffa0
Fotis Pegios <fotispeg@gmail.com>
2020-05-20 19:23:02 +0300

    ci/cd update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 
-const app = express();
-app.use(serveStatic(__dirname + "/dist"))
+const app = express()
+// static file serve
+app.use(express.static(`${__dirname}/dist`))
+// not found in static files, so default to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 const port = process.env.PORT || 5000
 app.listen(port)
 
-console.log('server started '+ port)
+console.log('\n=====================================')
+console.log('Server started in port: ', port)
+console.log(`Local Server: http://localhost:${port}`)
+console.log('=====================================')
